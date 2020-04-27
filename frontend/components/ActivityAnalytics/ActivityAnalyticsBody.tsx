1493bb124fdd211bc1efcad1fd1811ecddd8f01b
Tian Ling <general@codeling.me>
2020-04-27 21:23:33 +1000

    Added basic layout for activity information

--- frontend/components/ActivityAnalytics/ActivityAnalyticsBody.tsx
+++ frontend/components/ActivityAnalytics/ActivityAnalyticsBody.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import ActivityFileUploader from 'Components/common/ActivityFileUploader';
+import { Container, Row, Col } from 'react-bootstrap';
 
 type ActivityAnalyticsProps = {
   setImportFile: React.Dispatch<React.SetStateAction<File>>;
@@ -11,6 +12,22 @@ const ActivityAnalyticsBody: FunctionComponent<ActivityAnalyticsProps> = ({
   return (
     <div className="activity-analytics-body">
       <ActivityFileUploader className="activity-analytics-file" setImportFile={setImportFile}></ActivityFileUploader>
+      <Container className="activity-information-conatiner">
+        <Row>
+          <Col md={{ span: 3 }}>
+            <span className="activity-field-label">Power</span>
+            <span className="activity-field-value">Power Value</span>
+          </Col>
+          <Col md={{ span: 3 }}>
+            <span className="activity-field-label">Average Power</span>
+            <span className="activity-field-value">Average Power Value</span>
+          </Col>
+          <Col md={{ span: 3 }}>
+            <span className="activity-field-label">Heart Rate</span>
+            <span className="activity-field-value">Heart Rate Value</span>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
