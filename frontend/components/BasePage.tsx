c8452959d2f8fd22067b8038142e3f8358f6471c
Tian Ling <general@codeling.me>
2020-08-24 20:33:08 +1000

    Removed activity editor references

--- frontend/components/BasePage.tsx
+++ frontend/components/BasePage.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent } from 'react';
 import ActivityAnalytics from './ActivityAnalytics/ActivityAnalytics';
-import ActivityEditor from './ActivityEditor/ActivityEditor';
 import 'Styles/basepage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { SideNav, SideNavLink } from './common/SideNav';
@@ -8,10 +7,7 @@ import { Switch, Route } from 'react-router-dom';
 import * as pathConstants from 'Constants/pathConstants';
 
 export const BasePage: FunctionComponent = () => {
-  const sideNavLinks: SideNavLink[] = [
-    { url: pathConstants.ANALYTICS_PATH, iconUri: '', name: 'Activity Graph' },
-    { url: pathConstants.EDITOR_PATH, iconUri: '', name: 'File Edit' },
-  ];
+  const sideNavLinks: SideNavLink[] = [{ url: pathConstants.ANALYTICS_PATH, iconUri: '', name: 'Activity Graph' }];
 
   return (
     <>
@@ -20,9 +16,6 @@ export const BasePage: FunctionComponent = () => {
         <Switch>
           <Route path={pathConstants.ANALYTICS_PATH}>
             <ActivityAnalytics></ActivityAnalytics>
-          </Route>
-          <Route path={pathConstants.EDITOR_PATH}>
-            <ActivityEditor></ActivityEditor>
           </Route>
         </Switch>
       </div>
--- frontend/constants/pathConstants.ts
+++ frontend/constants/pathConstants.ts
@@ -1,2 +1,1 @@
 export const ANALYTICS_PATH = '/analytics';
-export const EDITOR_PATH = '/editor';
