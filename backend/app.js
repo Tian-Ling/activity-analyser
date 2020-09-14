4151d878cf32b1a78d283f85d031f2a8e409272c
Tian Ling <general@codeling.me>
2020-09-14 17:29:01 +1000

    Config updates

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('public/dist'));
 app.use(require('./routes/base.js'));
 
-app.listen(port, () =>
-  console.log(`Activity Analyser Backend listening at http://localhost:${port}`)
-);
+app.listen(port, () => console.log(`Activity Analyser Backend listening at http://localhost:${port}`));
