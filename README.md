42d83e7fc5dee32a4ab90b762ad403fb2e08ac59
Tian Ling <general@codeling.me>
2020-09-16 12:36:41 +1000

    Added default home page. Updated readme.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # activity-analyser
 
 ## Overview
-Project built on a React and Node.js stack that allows viewing and manipulation of FIT files.
+
+Project built on a React and Node Express stack that allows viewing of FIT files.
--- frontend/components/BasePage.tsx
+++ frontend/components/BasePage.tsx
@@ -3,7 +3,7 @@ import ActivityAnalytics from './ActivityAnalytics/ActivityAnalytics';
 import 'Styles/basepage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { SideNav, SideNavLink } from './common/SideNav';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Redirect, Route } from 'react-router-dom';
 import * as pathConstants from 'Constants/pathConstants';
 
 export const BasePage: FunctionComponent = () => {
@@ -14,6 +14,9 @@ export const BasePage: FunctionComponent = () => {
       <SideNav links={sideNavLinks}></SideNav>
       <div className="main-content">
         <Switch>
+          <Route exact path="/">
+            <Redirect to={pathConstants.ANALYTICS_PATH}></Redirect>
+          </Route>
           <Route path={pathConstants.ANALYTICS_PATH}>
             <ActivityAnalytics></ActivityAnalytics>
           </Route>
